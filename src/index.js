e55697411172c302dd235838f762fab7ffd01bd0
Dul <dul31389@gmail.com>
2023-03-21 09:00:12 -0500

    empezando a trabajar en mi js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+/* eslint-disable no-console */
+
+//crear la funcion para dar click
+function realizarAdopcion() {
+  window.location.href = "http://127.0.0.1:5500/src/gracias.html";
+}
+//realizarAdopción se llama cuando se hace click en el boton adoptar y te dirige a la pag gracias.html
+document.getElementaryById("btn-adoptar").addEventListener("click",realizarAdopcion);
+
+
+
+// seleccionar el button VALIDAR
+const boton = document.getElementaryById("VALIDAR");
+// Agregar un event listener para el click
+boton.addEventListener("click", function() {
+  alert("Haz click en el boton");
+} );
+
+
+
 import validator from './validator.js';
 
 console.log(validator);
