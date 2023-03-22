890fd3c6b4d6cc526b948db9fea19f54ae4f7c5d
Dul31 <dul31389@gmail.com>
2023-03-22 00:14:09 -0500

    button ADOPTAR y VALIDAR TARJETA Y TENER REFERENCIA A GRACIAS.HTML

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-console */
+const newLocal = "http://127.0.0.1:5500/src/gracias.html";
 
 //crear la funcion para dar click
 function realizarAdopcion() {
-  window.location.href = "http://127.0.0.1:5500/src/gracias.html";
+  window.location.href = newLocal;
 }
 //realizarAdopción se llama cuando se hace click en el boton adoptar y te dirige a la pag gracias.html
 document.getElementaryById("btn-adoptar").addEventListener("click",realizarAdopcion);
 
-
-
 // seleccionar el button VALIDAR
-const boton = document.getElementaryById("VALIDAR");
-// Agregar un event listener para el click
-boton.addEventListener("click", function() {
-  alert("Haz click en el boton");
-} );
+function validarDonación() {
+  window.location.href=newLocal;
+}
+document.getElementById("VALIDAR").addEventListener("click",validarDonación)
 
 
 
