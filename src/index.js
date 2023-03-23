b36b823df8b782b9e5204e4475048d7e83f26e29
Dul <dul31389@gmail.com>
2023-03-23 12:58:24 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 /* eslint-disable no-console */
-const newLocal = "http://127.0.0.1:5500/src/gracias.html";
-
-//crear la funcion para dar click
-function realizarAdopcion() {
-  window.location.href = newLocal;
-}
-//realizarAdopción se llama cuando se hace click en el boton adoptar y te dirige a la pag gracias.html
-document.getElementaryById("btn-adoptar").addEventListener("click",realizarAdopcion);
-
-// seleccionar el button VALIDAR
-function validarDonación() {
-  window.location.href=newLocal;
-}
-document.getElementById("VALIDAR").addEventListener("click",validarDonación)
-
-
-
 import validator from './validator.js';
+
+//hacer un boton que se valide en el validator
+const boton = document.getElementaryById("VALIDAR");
+// Agregar un event listener para el click
+boton.addEventListener("click", function() {
+  alert("Hacer click");
+} );
 
 console.log(validator);
