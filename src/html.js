3831db087c9df0a63299de906423e0f3037845d1
Dul <dul31389@gmail.com>
2023-03-23 18:21:16 -0500

    commit

--- src/html.js
+++ src/html.js
@@ -2,13 +2,17 @@
 const saludo = "Bienvenid@ a la FundaciónPetsFriends Wasi";
     alert(saludo);
 
+const nombre= ("")
+nombre = prompt("Ingresa tu nombre", nombre);
+
+
 
 //hacer referencia del button adoptar a una url
-const newLocal = "http://127.0.0.1:5500/src/gracias.html";
+const paginaWeb = "http://127.0.0.1:5500/src/gracias.html";
 
 //crear la funcion para dar click
 function realizarAdopcion() {
-  window.location.href = newLocal;
+  window.location.href = paginaWeb;
 }
 //realizarAdopción se llama cuando se hace click en el boton adoptar y te dirige a la pag gracias.html
 document.getElementaryById("btn-adoptar").addEventListener("click",realizarAdopcion);
