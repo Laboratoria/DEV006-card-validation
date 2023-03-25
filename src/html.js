1a1fc713deab2ce298b70dd51b17ac42b6712d63
Dul <dul31389@gmail.com>
2023-03-24 19:53:02 -0500

    edicion de imágenes

--- src/html.js
+++ src/html.js
@@ -5,8 +5,3 @@ alert(saludo);
 // un texto y un valor en este caso nombre
 prompt("Por favor, ingresa tu nombre"+ nombre);
 
-const adoptar = document.getElementaryById("btn-adopta");
-// Agregar un event listener para el click
-adoptar.addEventListener("click", function() {
-  window.location.href= "gracias.html";
-} );
