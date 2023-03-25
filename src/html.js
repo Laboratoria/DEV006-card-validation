fd8c3ffa3d00ae3642aaf911eeaa01778f12fa3b
Dul <dul31389@gmail.com>
2023-03-24 19:39:27 -0500

    añadí una imagen en contactanos.html

--- src/html.js
+++ src/html.js
@@ -1,17 +1,12 @@
 //saludo al visitante con un alert
-const saludo = "Bienvenid@ a la Fundación PetsFriends Wasi",
-      nombre = ("");
-    alert(saludo);
-    //agregando un texto y un valor en este caso nombre
-nombre = prompt("Por favor, ingresa tu nombre"+ nombre);
+const saludo = "Bienvenid@ a la Fundación PetsFriends Wasi";
+const nombre = "";
+alert(saludo);
+// un texto y un valor en este caso nombre
+prompt("Por favor, ingresa tu nombre"+ nombre);
 
-
-//hacer referencia del button adoptar a una url
-const paginaWeb = "http://127.0.0.1:5500/src/gracias.html";
-
-//crear la funcion para dar click
-function realizarAdopcion() {
-  window.location.href = paginaWeb;
-}
-//realizarAdopción se llama cuando se hace click en el boton adoptar y te dirige a la pag gracias.html
-document.getElementaryById("btn-adoptar").addEventListener("click",realizarAdopcion);
+const adoptar = document.getElementaryById("btn-adopta");
+// Agregar un event listener para el click
+adoptar.addEventListener("click", function() {
+  window.location.href= "gracias.html";
+} );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-console */
 import validator from './validator.js';
-
 //hacer un boton que se valide en el validator
 const boton = document.getElementaryById("VALIDAR");
 // Agregar un event listener para el click
 boton.addEventListener("click", function() {
-  alert("Hacer click");
+  window.location.href= "gracias.html";
+} );
+
+const adoptar = document.getElementaryById("btn-adopta");
+// Agregar un event listener para el click
+adoptar.addEventListener("click", function() {
+  window.location.href= "gracias.html";
 } );
 
 console.log(validator);
