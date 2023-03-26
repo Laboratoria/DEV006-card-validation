6ada1df66774f780f10435e5f8942758232b88a0
Dul <dul31389@gmail.com>
2023-03-26 03:11:58 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ btnAbrir.addEventListener('click', ()=>{
 //Para agregar los DÍAS al label= selectMes
 //agregamos un ciclo FOR para crear las 12 opciones que son los meses del año
 for(let i = 1; i <= 12; i++){
-  let opcion = document.createElement('option');
+  const opcion = document.createElement('option');
   opcion.value = i;
   opcion.innerText = i;
   formulario.selectMes.appendChild(opcion);
@@ -30,10 +30,17 @@ const yearActual = new Date().getFullYear();
 //se agrega ciclo FOR para que indique los años de expiracion
 // yearActual + 4 para que solo muestre hasta 4 años despues del actual
 for(let i = yearActual; i <= yearActual + 4; i++){
-  let opcion = document.createElement('option');
-  opcion.value = i;
-  opcion.innerText = i;
-  formulario.selectYear.appendChild('opcion');
+  const opcion2 = document.createElement('option2');
+  opcion2.value = i;
+  opcion2.innerText = i;
+  formulario.selectYear.appendChild('opcion2');
 }
+
+//inputNumero =>> Numero de Tarjeta
+//el input va a recibir un evento cuando se presiono una letra
+formulario.inputNumero.addEventListener('keyup', (e) => {
+  let valorInput = e.target.value;
+  formulario.inputNumero.value = valorInput.replace(/\D/g, '');
+});
 
 
