78f7705d8804ebb73174c3e4f83d367910b1ebae
Dul <dul31389@gmail.com>
2023-03-25 21:10:55 -0500

    commit

--- src/html.js
+++ src/html.js
@@ -5,3 +5,15 @@ alert(saludo);
 // un texto y un valor en este caso nombre
 prompt("Por favor, ingresa tu nombre"+ nombre);
 
+//hacer un boton que se valide en el validator
+//const boton = document.getElementaryById("VALIDAR");
+// Agregar un event listener para el click
+//boton.addEventListener("click", function() {
+//window.location.href= "gracias.html";
+//} );
+
+//const adoptar = document.getElementaryById("btn-adopta");
+// Agregar un event listener para el click
+//adoptar.addEventListener("click", function() {
+//window.location.href= "gracias.html";
+//} );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-/* eslint-disable no-console */
 import validator from './validator.js';
-//hacer un boton que se valide en el validator
-const boton = document.getElementaryById("VALIDAR");
-// Agregar un event listener para el click
-boton.addEventListener("click", function() {
-  window.location.href= "gracias.html";
-} );
 
-const adoptar = document.getElementaryById("btn-adopta");
-// Agregar un event listener para el click
-adoptar.addEventListener("click", function() {
-  window.location.href= "gracias.html";
-} );
+const tarjeta = document.querySelector('#tarjeta');
+tarjeta.addEventListener('click', () => {
+  tarjeta.classList.toggle('active');
+});
+
+
 
 console.log(validator);
