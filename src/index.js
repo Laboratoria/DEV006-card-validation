a877ffcd96f45a6d2e5ba28de1ef29b91a7c6f1b
Dul <dul31389@gmail.com>
2023-03-25 21:32:27 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import validator from './validator.js';
-
 const tarjeta = document.querySelector('#tarjeta');
+//rotar la tarjeta con un clickA
 tarjeta.addEventListener('click', () => {
   tarjeta.classList.toggle('active');
 });
+
+
+const btnAbrir = document.querySelector('#btnAbrir');
+const formulario = document.querySelector('#formulario-tarjeta');
+//para abrir el formulario
+btnAbrir.addEventListener('click', ()=>{
+  btnAbrir.classList.toggle('active');
+  formulario.classList.toggle('active');
+});
+
 
 
 
