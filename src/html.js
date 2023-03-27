fe1175bfe2afa50708516f776e2d2cc5b1bc9379
Dul <dul31389@gmail.com>
2023-03-27 01:11:56 -0500

    mostrar tarjeta al frente en js

--- src/html.js
+++ src/html.js
@@ -3,14 +3,7 @@ const saludo = "Bienvenid@ a la Fundación PetsFriends Wasi";
 const nombre = "";
 alert(saludo);
 // un texto y un valor en este caso nombre
-prompt("Por favor, ingresa tu nombre"+ nombre);
-
-//hacer un boton que se valide en el validator
-//const boton = document.getElementaryById("VALIDAR");
-// Agregar un event listener para el click
-//boton.addEventListener("click", function() {
-//window.location.href= "gracias.html";
-//} );
+prompt("Por favor, ingresa tu nombre" + nombre);
 
 //const adoptar = document.getElementaryById("btn-adopta");
 // Agregar un event listener para el click
--- src/index.js
+++ src/index.js
@@ -10,37 +10,81 @@ tarjeta.addEventListener('click', () => {
 // addEventListener para abrir, botón abrir el formulario
 const btnAbrir = document.querySelector('#btnAbrir');
 const formulario = document.querySelector('#formulario-tarjeta');
-btnAbrir.addEventListener('click', ()=>{
+btnAbrir.addEventListener('click', () => {
   btnAbrir.classList.toggle('active');
   formulario.classList.toggle('active');
 });
 
 //Para agregar los DÍAS al label= selectMes
 //agregamos un ciclo FOR para crear las 12 opciones que son los meses del año
-for(let i = 1; i <= 12; i++){
-  const opcion = document.createElement('option');
+for (let i = 1; i <= 12; i++) {
+  let opcion = document.createElement('option');
   opcion.value = i;
   opcion.innerText = i;
   formulario.selectMes.appendChild(opcion);
 }
 
-//seleccionar el año
-//se crea la variable yearActual
-const yearActual = new Date().getFullYear();
 //se agrega ciclo FOR para que indique los años de expiracion
-// yearActual + 4 para que solo muestre hasta 4 años despues del actual
-for(let i = yearActual; i <= yearActual + 4; i++){
-  const opcion2 = document.createElement('option2');
-  opcion2.value = i;
-  opcion2.innerText = i;
-  formulario.selectYear.appendChild('opcion2');
+//yearActual + 4 para que solo muestre hasta 4 años despues del actual
+const yearActual = new Date().getFullYear();
+for (let i = yearActual; i <= yearActual + 4; i++) {
+  let opcion = document.createElement('option');
+  opcion.value = i;
+  opcion.innerText = i;
+  formulario.selectYear.appendChild(opcion);
 }
 
-//inputNumero =>> Numero de Tarjeta
-//el input va a recibir un evento cuando se presiono una letra
+//inputNumero
+//para reemplazar lo que se escribe en los input y también se copie en la tarjeta de gatito
+const numeroTarjeta = document.querySelector('#tarjeta .numero');
+//se accede al formulario, luego inputNumero y se reemplazan los valores
 formulario.inputNumero.addEventListener('keyup', (e) => {
   let valorInput = e.target.value;
-  formulario.inputNumero.value = valorInput.replace(/\D/g, '');
+
+  numeroTarjeta.textContent = valorInput;
+  //si el usuario borra el numero de tarjeta y el espacio no se quede en blanco hacemos un if
+  //condicion de que se complete como estaba anteriormente
+  if (valorInput === '') {
+    numeroTarjeta.textContent = "#### #### #### ####";
+  }
+
+  //volteamos la tarjeta para q el usuario vea el frente
+  mostrarFrente();
 });
 
+//inputNombre 
+const nombreTarjeta = document.querySelector('#tarjeta .nombre');
+formulario.inputNombre.addEventListener('keyup', (e) => {
+  let valorInput = e.target.value;
 
+  //para que detecte numeros y no se puedan escribir
+  formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+  //para que se copien los valores en la tarjeta 
+  nombreTarjeta.textContent = valorInput;
+  if (valorInput === '') {
+    nombreTarjeta.textContent = "Ej. Nacdul Ramirez";
+  }
+  mostrarFrente();
+});
+
+//para voltear la tarjeta y mostrar la cara del frente
+const mostrarFrente = () => {
+  //hacer un if para comprobar si la tarjeta contiene el active y sele agrega el remove para que quite la clase y se votee
+  if (tarjeta.classList.contains('active')) {
+    tarjeta.classList.remove('active');
+  }
+}
+
+// Mes
+const mesExpiracion = document.querySelector('#tarjeta #expiracion .mes');
+formulario.selectMes.addEventListener('change', (e) => {
+  mesExpiracion.textContent = e.target.value;
+  mostrarFrente();
+});
+
+//año
+const yearExpiracion = document.querySelector('#tarjeta #expiracion .year');
+formulario.selectYear.addEventListener('change', (e) => {
+  yearExpiracion.textContent = e.target.value.slice(2); //slice(2) para que nos permita ver los 2 ultimos digitos del año
+  mostrarFrente();
+})
