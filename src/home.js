74b3724b2c5f2c25e67e5e347b1c34abdbe425ae
Dul Ramirez <dul31389@gmail.com>
2024-02-21 21:47:39 -0500

    cambiando de contenedor a las imagenes

--- src/home.js
+++ src/home.js
@@ -6,3 +6,31 @@ closeButton.addEventListener('click', function() {
   popup.style.display = 'none';
 });
 
+// slide
+document.addEventListener("DOMContentLoaded", function() {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".imagen");
+
+  function showSlide() {
+    slides.forEach((slide, index) => {
+      if (index === currentSlide) {
+        slide.style.opacity = 1;
+      } else {
+        slide.style.opacity = 0;
+      }
+    });
+
+    currentSlide = (currentSlide + 1) % slides.length;
+  }
+
+  function toggleSlides() {
+    setInterval(function() {
+      showSlide();
+      setTimeout(() => {
+        showSlide();
+      }, 2000); // Cambiar de slide cada 2 segundos
+    }, 6000); // Iniciar cambio de slides cada 6 segundos
+  }
+
+  toggleSlides();
+});
