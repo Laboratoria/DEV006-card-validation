b1a089d7b6605ec1eba08cce5673fbce62d1def5
Dul Ramirez <dul31389@gmail.com>
2024-02-23 21:44:45 -0500

    modificando el formulario de adopcion

--- src/home.js
+++ src/home.js
@@ -6,3 +6,20 @@ closeButton.addEventListener('click', function() {
   popup.style.display = 'none';
 });
 
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+  const dobInput = document.getElementById("hb");
+  const dob = new Date(dobInput.value);
+  const eighteenYearsAgo = new Date();
+  eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
+
+  if (dob <= eighteenYearsAgo) {
+    // Si el usuario es mayor de edad, se envía el formulario a "gracias.html"
+    this.submit();
+  } else {
+    // Si el usuario no es mayor de edad, se muestra un mensaje de alerta
+    alert("Debes ser mayor de edad para enviar el formulario.");
+  }
+});
